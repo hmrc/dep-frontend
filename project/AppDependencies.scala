e9b9ccb911b74074bc8dff1d00e7b066f592ffff
Daniel Manning <daniel-manning@users.noreply.github.com>
2020-01-22 07:40:31 +0000

    add_whitelist: Add whitelist filter (#39)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,8 @@ object AppDependencies {
 
     "uk.gov.hmrc" %% "govuk-template" % "5.47.0-play-26",
     "uk.gov.hmrc" %% "play-ui" % "8.5.0-play-26",
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0"
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0",
+    "uk.gov.hmrc" %% "play-whitelist-filter" % "3.1.0-play-26"
   )
 
   val test = Seq(
@@ -17,9 +18,10 @@ object AppDependencies {
     "org.scalatest" %% "scalatest" % "3.0.8" % "test",
     "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
     "org.jsoup" % "jsoup" % "1.10.2" % "test",
+    "org.mockito" % "mockito-all" % "1.10.19" % "test",
     "com.typesafe.play" %% "play-test" % current % "test",
-    "org.pegdown" % "pegdown" % "1.6.0" % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % "test, it"
+    "org.pegdown" % "pegdown" % "1.6.0" % "test",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % "test"
   )
 
 }
